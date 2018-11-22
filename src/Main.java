c9f98c6ae5160e68290d66e4b7e291e539131c27
Patricio Quiroz <pmquirozvargas@gmail.com>
2018-11-22 17:11:57 -0300

    Trying make population with learning genetic

--- src/Main.java
+++ src/Main.java
@@ -8,30 +8,31 @@ import java.util.List;
 
 public class Main {
 
-    public static final int POPULATION_SIZE = 10;
-    public static final int QUANTITY_PARENTS = 5;
+    public static final int POPULATION_SIZE = 1000;
+    public static final int QUANTITY_PARENTS = 500;
     public static final String REFERENCE_SEQUENCE = "10010";
     public static final double MUTATION_RATE = 0.2;
 
     public static void main(String[] args) {
         PopulationManager populationManager = new PopulationManager();
-        Population bestPopulation = null;
 
         List<Population> resumeTotalPopulation = new ArrayList<>();
 
-        long timeInit = System.currentTimeMillis();
-        for(int i = 1; i <= 100; i++){
+        Population initPopulation = populationManager.makePopulation(POPULATION_SIZE, REFERENCE_SEQUENCE); //TODO change reference 'sequence' to generic reference
+        Population bestPopulation = initPopulation;
 
-            Population population = populationManager.makePopulation(POPULATION_SIZE, REFERENCE_SEQUENCE); //TODO change reference 'sequence' to generic reference
-            List<Member> parents = populationManager.getParentsMemberList(population.getPopulationList(), QUANTITY_PARENTS);
+        long timeInit = System.currentTimeMillis();
+        int i = 1;
+        while(i <= 1000 || bestPopulation.getQuantityBestMembers() > 500){
+
+            List<Member> parents = populationManager.getParentsMemberList(bestPopulation.getPopulationList(), QUANTITY_PARENTS);
             Population newPopulation = populationManager.reproduction(parents, POPULATION_SIZE, MUTATION_RATE, REFERENCE_SEQUENCE);
 
-            if((bestPopulation == null) || bestPopulation.getQuantityBestMembers() < newPopulation.getQuantityBestMembers()){
+            if(bestPopulation.getQuantityBestMembers() < newPopulation.getQuantityBestMembers()){
                 bestPopulation = newPopulation;
             }
-
+            i++;
             resumeTotalPopulation.add(newPopulation);
-            System.out.println("index population="+i+" best members="+ newPopulation.getQuantityBestMembers());
         }
         long duration = System.currentTimeMillis() - timeInit;
         System.out.printf("Best population. Total population=%d, best members=%d, time=%d", POPULATION_SIZE,bestPopulation.getQuantityBestMembers(), duration);
