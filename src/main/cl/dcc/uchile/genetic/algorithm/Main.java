f90b3baf2492f15f6a8fc60e4caf1476cdf4cf32
Patricio Quiroz <pmquirozvargas@gmail.com>
2018-11-30 16:47:46 -0300

    Guess new word

--- src/main/cl/dcc/uchile/genetic/algorithm/Main.java
+++ src/main/cl/dcc/uchile/genetic/algorithm/Main.java
@@ -9,34 +9,33 @@ import java.util.List;
 
 public class Main {
 
-    public static final int POPULATION_SIZE = 1000;
-    public static final int QUANTITY_PARENTS = 500;
-    public static final int NUMBER_ATTEMPTS = 350;
-    public static final String REFERENCE_SEQUENCE = "hola";
+    public static final int POPULATION_SIZE = 100;
+    public static final int QUANTITY_PARENTS = 50;
+    public static final int NUMBER_ATTEMPTS = 75;
+    public static final String REFERENCE_SEQUENCE = "estacionamie";
     public static final double MUTATION_RATE = 0.4;
 
     public static void main(String[] args) {
         PopulationManager populationManager = new PopulationManager();
 
         Generation firstGeneration = populationManager.makeFirstGeneration(POPULATION_SIZE, REFERENCE_SEQUENCE);
-        Generation chosenGenration = firstGeneration;
+        Generation chosenGeneration = firstGeneration;
         List<Generation> generationList = new ArrayList<>();
         generationList.add(firstGeneration);
 
-
         int index = 0;
         do {
-            List<Member> parents = populationManager.getParentsMemberList(chosenGenration.getPopulationList(), QUANTITY_PARENTS, NUMBER_ATTEMPTS);
+            List<Member> parents = populationManager.getParentsMemberList(chosenGeneration.getPopulationList(), QUANTITY_PARENTS, NUMBER_ATTEMPTS);
             Generation newGeneration = populationManager.reproduction(parents, POPULATION_SIZE, MUTATION_RATE, REFERENCE_SEQUENCE);
 
-            if(newGeneration.getBestMember().getFitness() > chosenGenration.getBestMember().getFitness()){
-                chosenGenration = newGeneration;
+            if(newGeneration.getBestMember().getFitness() > chosenGeneration.getBestMember().getFitness()){
+                chosenGeneration = newGeneration;
             }
 
             generationList.add(newGeneration);
             index++;
-        } while(chosenGenration.getBestMember().getFitness() < REFERENCE_SEQUENCE.length() && index < 1000);
+        } while(chosenGeneration.getBestMember().getFitness() < REFERENCE_SEQUENCE.length() && index < 1000);
 
-        System.out.println("generations: "+ generationList.size()+", best member: "+chosenGenration.getBestMember());
+        System.out.println("generations: "+ generationList.size()+", best member: "+chosenGeneration.getBestMember());
     }
 }
