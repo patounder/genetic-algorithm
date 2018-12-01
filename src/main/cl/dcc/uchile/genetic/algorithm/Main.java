58f86eaaa7bace3fc307e71240d9e7ee9c534fba
Patricio Quiroz <pmquirozvargas@gmail.com>
2018-12-01 01:00:44 -0300

    set correct parameter for create new generation

--- src/main/cl/dcc/uchile/genetic/algorithm/Main.java
+++ src/main/cl/dcc/uchile/genetic/algorithm/Main.java
@@ -13,12 +13,13 @@ public class Main {
     public static final int QUANTITY_PARENTS = 50;
     public static final int NUMBER_ATTEMPTS = 75;
     public static final String REFERENCE_SEQUENCE = "estacionamie";
+    public static final int MATRIX_LENGTH = 4;
     public static final double MUTATION_RATE = 0.4;
 
     public static void main(String[] args) {
         PopulationManager populationManager = new PopulationManager();
 
-        Generation firstGeneration = populationManager.makeFirstGeneration(POPULATION_SIZE, REFERENCE_SEQUENCE);
+        Generation firstGeneration = populationManager.makeFirstGeneration(POPULATION_SIZE, MATRIX_LENGTH);
         Generation chosenGeneration = firstGeneration;
         List<Generation> generationList = new ArrayList<>();
         generationList.add(firstGeneration);
